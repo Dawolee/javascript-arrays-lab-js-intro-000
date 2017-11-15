513fa0fbc26d0150ad2bdd177ee511cdbc1f4450
Da Woon Lee <daniel.lee9415@gmail.com>
2017-11-15 22:11:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,26 @@ function destructivelyAppendKitten(name) {
   kittens.push(name);
 }
 
-function destrucivelyPrependKitten(name) {
+function destructivelyPrependKitten(name) {
   kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+function appendKitten(name) {
+  return [name, ...kittens];
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1);
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1);
 }
